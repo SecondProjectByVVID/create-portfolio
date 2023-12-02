94736022d8e409a7db0a2328f407ae4659aa96f7
Даниил <mon.inch@yandex.ru>
2023-12-02 17:01:37 +0300

    коммит от димы вроде бы с авторизацией)

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,14 +1,23 @@
 from rest_framework import serializers
-from .models import User, Portfolio
+from .models import User, Profile, Portfolio
 from project import settings
+from phonenumber_field.serializerfields import PhoneNumberField
 
-# from allauth.account.adapter import get_adapter
-# from allauth.account.utils import setup_user_email
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    mobile = PhoneNumberField()
+    
     class Meta:
         model = User
+        fields = ('id', 'email', 'mobile', 'surname', 'name', 'profession', 'password',)
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    class Meta:
+        model = Profile
         fields = ('__all__')
 
 

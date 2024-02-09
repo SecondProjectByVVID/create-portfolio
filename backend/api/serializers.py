b520cf8435a7245311057fd607852057e615d5cf
moninch <mon.inch@yandex.ru>
2024-02-09 23:40:51 +0300

    Сделал pep8(дима гей не соблюдает стандарты)

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,11 +3,13 @@ from .models import Profile, Portfolio
 from project import settings
 from users.serializers import UserListSerializer
 
+
 class ProfileSerializer(serializers.ModelSerializer):
     user = UserListSerializer()
     class Meta:
         model = Profile
         fields = ('__all__')
+
 
 class PortfolioSerializer(serializers.ModelSerializer):
     user = UserListSerializer()

dc25907ccc0bca00b9d883f9f02c20bd9db0ff94
HarDims <dima.khil.00@mail.ru>
2024-02-21 22:13:15 +0300

    Усовершенствовал профиль, начало разработки чата

--- backend/project/urls.py
+++ backend/project/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
     path('', include('users.urls')),
+    path('chat/', include("chat.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
